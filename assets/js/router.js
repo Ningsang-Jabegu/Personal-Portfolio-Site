1f8a515b246763a5e4434df5a3abe32fed910cd7
Ningsang Jabegu <86409265+Ningsang-Jabegu@users.noreply.github.com>
2023-12-26 18:09:16 +0545

    second blog post uploaded

---
+++ assets/js/router.js
@@ -0,0 +1,11 @@
+document.getElementById('about-btn').addEventListener('click', function() {
+    document.getElementById('content').innerHTML = '<h1>About Page</h1>';
+    // Update meta tags here
+  });
+  
+  document.getElementById('resume-btn').addEventListener('click', function() {
+    document.getElementById('content').innerHTML = '<h1>Resume Page</h1>';
+    // Update meta tags here
+  });
+  
+  // Add event listeners for the other buttons 
